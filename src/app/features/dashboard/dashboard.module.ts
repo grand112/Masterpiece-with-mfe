4e63ef353307fd48d683bc722da4b8784cd293a4
Łukasz Bugajski <lukaszbugajski1998@gmail.com>
2022-02-01 23:11:23 +0100

    add dashboard scaffolding

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { EditorComponent } from './components/editor/editor.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
 import { TuiImageEditorModule } from 'tui-image-editor-angular';
-import { EditorComponent } from './components/editor/editor.component';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    DashboardComponent,
-    EditorComponent
+    EditorComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule,
+    MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    DashboardRoutingModule,
     TuiImageEditorModule,
     TuiImageEditorModule,
+    MatSidenavModule
   ]
 })
 export class DashboardModule { }
